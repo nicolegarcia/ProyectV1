b9b3ba35fdc9c80eaa22ac5281331cd73d74e1a8
Francisco Cabezas <francisco.cabezas@accedo.tv>
2015-12-03 19:20:22 -0300

    - Se corrigen imports y algunos errores declarativos

--- app/src/main/java/com/example/nicole/proyectv1/MainFragment.java
+++ app/src/main/java/com/example/nicole/proyectv1/MainFragment.java
@@ -1,5 +1,7 @@
 package com.example.nicole.proyectv1;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +13,18 @@ import com.facebook.FacebookSdk;
 public class MainFragment extends Fragment {
 
     public MainFragment(){
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getActivity().getAplicationContext);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
     }
 
     @Override
-    public View onCreateView(LayoutInflater, ViewGroup container, Bundle savedInstanceState){
-        return  inflater.inflater(R.layout.fragment_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
 }
