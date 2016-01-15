c59f1dc4d7fa188a7f7bab7524d3c1a405e2bc8c
nicolegarcia <nienna.elras@gmail.com>
2016-01-15 10:58:57 -0300

    D:

---
+++ app/src/main/java/com/example/nicole/proyectv1/Ingredientes/Ingrediente.java
@@ -0,0 +1,27 @@
+package com.example.nicole.proyectv1.Ingredientes;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nicole on 15-01-2016.
+ */
+public class Ingrediente extends Object{
+
+    String ingrediente;
+
+    public Ingrediente(){
+
+    }
+
+    public Ingrediente(String ingrediente)
+    {
+        this.ingrediente = ingrediente;
+    }
+
+    public String getIngrediente() {
+
+        return ingrediente;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nicole/proyectv1/Ingredientes/IngredientesActivity.java
@@ -0,0 +1,23 @@
+package com.example.nicole.proyectv1.Ingredientes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.nicole.proyectv1.R;
+
+
+/**
+ * Created by nicole on 15-01-2016.
+ */
+public class IngredientesActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.despensa);
+
+
+    }
+}
